4dfbaa2b513554556615b0175203d0386e67c1bf
常帅 <changshuai@renrenche.com>
2020-06-24 15:27:09 +0800

    更新test

--- src/com/shuai/Test.java
+++ src/com/shuai/Test.java
@@ -4,21 +4,10 @@ package com.shuai;
 public class Test {
 
     public static void main(String[] args) {
-        byte b = 21;
-        String method = method(b, false);
-        System.out.println(method);
 
     }
 
 
-    public static String method(byte b, boolean flag) {
-        if (flag) {
-            return String.format("%8s", Integer.toBinaryString(b & 0xFF)).replace(' ', '0');
-        } else {
-            return String.format("%s", Integer.toBinaryString(b));
-        }
-
-    }
 }
 
 
