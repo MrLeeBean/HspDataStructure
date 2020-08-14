ed0892584893d37deb6694fdaa03131d9f39e623
常帅 <changshuai@renrenche.com>
2020-08-14 18:55:10 +0800

    普利姆算法done

--- src/com/shuai/_algorithm/prim/PrimTest.java
+++ src/com/shuai/_algorithm/prim/PrimTest.java
@@ -46,16 +46,16 @@ class Graph {
 
     //展示图的邻接矩阵
     public void show() {
-        System.out.println("--------------------------------------------------");
         if (matrix != null) {
-            for (int i = 0; i < matrix.length; i++) {
-                for (int j = 0; j < matrix[0].length; j++) {
+            System.out.println();
+            for (int i = 0; i < data.length; i++) {
+                for (int j = 0; j < data.length; j++) {
                     System.out.printf("%7d", matrix[i][j]);
                 }
                 System.out.println();
             }
+            System.out.println();
         }
-        System.out.println("--------------------------------------------------");
     }
 
     /**
